b394ac2c4d8b19f5fce683d77d4645cd3bb95590
Amr Eniou <amr@ofload.com>
2020-09-13 22:13:41 +0200

    clean up

--- test/uhf_plugin_test.dart
+++ test/uhf_plugin_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:uhf_rt510_plugin/uhf_rt510_plugin.dart';
+import 'package:rt510_plugin/uhf_rt510_plugin.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('uhf_plugin');
